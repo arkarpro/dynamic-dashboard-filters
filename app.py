6ddaaa819a8beeebaad1c92736fa36111622d942
Arkar Linn <arkarlinn1988@gmail.com>
2025-04-28 06:04:11 +0630

    tested git via vscode

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-import pandas as pd
-import streamlit as st
+import pandas as pd   #inport pandas
+import streamlit as st #import streamlist to show fig target to enduser
+df_org = pd.read_csv("populationdata.csv") #read csv by using pandas
 
-df = pd.read_csv("populationdata.csv")
+st.markdown("## Step-by-Step Dynamic-Dashboard-Filters") #added a title
+st.info("***please check below table with scorlable for the original data at below table, you can download it***") #add an info
+st.dataframe(df_org, height= 200, width=800) #put data frame size h200, w800
 
-st.markdown("## Step-by-Step Dynamic-Dashboard-Filters")
-st.dataframe(df)
+df = df_org.copy() # Always work on a copy
+
+
